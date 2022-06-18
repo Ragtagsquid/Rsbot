cf45d600fd534997e863af09ff07c2991e69669c
Joshua M <joshua.muhammad928@gmail.com>
2022-06-17 20:08:46 -0700

    added drop function

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ def isInventoryOpen():
 
     else:
         print("cant find Inventory on screen")
+
+def drop():
+    tuple = myGui.locateOnScreen("./images/copper_ore.png",confidence = 0.7)
+    cords = myGui.center(tuple)
+    myGui.rightClick(cords)
+    tuple = myGui.locateOnScreen("./images/drop_copper_ore.png", confidence = 0.7)
+    cords = myGui.center(tuple)
+    myGui.click(cords)
+    print("dropped ore")
